4fcb9b77a9fa792daeba2b705b9ac7bec678e587
mahaveer94 <mg1chandel@gmail.com>
2024-08-22 10:37:53 +0530

    added model file

--- app.py
+++ app.py
@@ -58,5 +58,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    #app.run(debug=True)  #for local run
-    app.run(host='0.0.0.0', port=5000) #for deployment run
+    app.run(debug=True)  #for local 
+    #app.run(host='0.0.0.0', port=5000) #for deployment
